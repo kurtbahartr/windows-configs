5eb814240d7486c64fed74d0ab2aad37f0cf934c
Bahar KURT <168406273+kurtbahartr@users.noreply.github.com>
2024-12-08 18:56:23 +0000

    Upload the actual files

---
+++ winget-bundle.cmd
@@ -0,0 +1,12 @@
+winget update --accept-source-agreements
+start winget install "Samsung Account" --source msstore --accept-package-agreements
+start winget install 7zip.7zip --source winget
+start winget install Discord.Discord --source winget
+start winget install "Python 3.11" --source msstore --accept-package-agreements
+start winget install "Speedtest by Ookla" --source msstore --accept-package-agreements
+REM Spotify - Music and Podcasts
+start winget install 9NCBCSZSJRSB --source msstore --accept-package-agreements
+start winget install Valve.Steam --source winget
+start winget install "Unigram" --source msstore --accept-package-requirements
+start winget install Microsoft.WindowsTerminal --source winget
+start winget install "WhatsApp" --source msstore --accept-package-agreements
