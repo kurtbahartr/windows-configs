40515b7c53868985d7f0743234360bc51343acdf
Bahar Kurt <kurtbahartr@users.noreply.github.com>
2024-12-10 11:37:50 +0300

    Add Git CLI and GitHub CLI into winget-bundle-optional
    Also refine the way winget-bundle-all is handled.

--- winget-bundle-all.cmd
+++ winget-bundle-all.cmd
@@ -1,29 +1,6 @@
-winget update --accept-source-agreements
-start winget install "Samsung Account" --source msstore --accept-package-agreements
-start winget install 7zip.7zip --source winget
-start winget install Discord.Discord --source winget
-start winget install "Python 3.11" --source msstore --accept-package-agreements
-start winget install "Speedtest by Ookla" --source msstore --accept-package-agreements
-REM Spotify - Music and Podcasts
-start winget install 9NCBCSZSJRSB --source msstore --accept-package-agreements
-start winget install Valve.Steam --source winget
-start winget install "Unigram" --source msstore --accept-package-requirements
-start winget install Microsoft.WindowsTerminal --source winget
-start winget install "WhatsApp" --source msstore --accept-package-agreements
-start winget install Google.Chrome --source winget
-start winget install Windscribe.Windscribe --source winget
-start winget install "Samsung Cloud Assistant" --source msstore --accept-package-agreements
-start winget install "Microsoft To Do" --source msstore --accept-package-agreements
-start winget install Microsoft.PowerToys --source winget
-start winget install Oracle.VirtualBox --source winget
-start winget install "Quick Share" --source msstore --accept-package-agreements
-start winget install Google.QuickShare --source winget
-start winget install "Rufus" --source msstore --accept-package-agreements
-start winget install "Samsung Gallery" --source msstore --accept-package-agreements
-start winget install "Slack" --source msstore --accept-package-agreements
-start winget install StartIsBack.StartAllBack --source winget
-REM start cmd /C "winget install StartIsBack.StartAllBack --source winget && start sib-activate:XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
-start winget install tailscale.tailscale --source winget
-start winget install CodeSector.TeraCopy --source winget
-start cmd /C "winget install Microsoft.VCRedist.2015+.x64 --source winget && winget install Transmission.Transmission --source winget"
-start winget install VideoLAN.VLC --source winget
+curl --remote-name https://raw.githubusercontent.com/kurtbahartr/windows-configs/master/winget-bundle.cmd
+start /wait cmd /C winget-bundle.cmd
+del winget-bundle.cmd
+curl --remote-name https://raw.githubusercontent.com/kurtbahartr/windows-configs/master/winget-bundle-optional.cmd
+start /wait cmd /C winget-bundle.cmd
+del winget-bundle.cmd
--- winget-bundle-optional.cmd
+++ winget-bundle-optional.cmd
@@ -1,4 +1,6 @@
 winget update --accept-source-agreements
+start winget install Git.Git --source winget
+start winget install GitHub.cli --source winget
 start winget install Google.Chrome --source winget
 start winget install Windscribe.Windscribe --source winget
 start winget install "Samsung Cloud Assistant" --source msstore --accept-package-agreements
