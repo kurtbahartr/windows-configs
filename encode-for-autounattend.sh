b5d50f98270003774df89d82023bf2db9f5a7182
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-07-18 22:30:12 +0300

    Handle Administrator account when generating password entry also

--- encode-for-autounattend.sh
+++ encode-for-autounattend.sh
@@ -3,11 +3,25 @@
 # TODO: Generate an appropriate portion for use in autounattend.xml also.
 
 # Get the password, also hide it from stdout since it's, well, a password.
-read -p "Enter password to encode for autounattend.xml:" -s string_to_encode
+read -p "Enter password to encode for autounattend.xml: " -s string_to_encode
 echo -ne "\n"
 
+# Ask if we're dealing with a password made for the Administrator account.
+read -p "Is this password for the account \"Administrator\" (y/N)? " -s admin_account
+echo -ne "\n"
+
+# Convert it to lowercase.
+admin_account=$(echo "$admin_account" | tr '[:upper:]' '[:lower:]')
+
+# If we're dealing with an administrator account...
+if [[ "$admin_account" == y* ]]; then
+  echo "Dealing with the Administrator account - Prepending \"Admin\"..."
+  string_to_encode+="Admin"
+else
+  echo "Not dealing with the Administrator account."
+fi
+
 # Concatenate the password with the word "Password".
-# TODO: Ask for the account type and change the phrase to concatenate to "AdminPassword" if it's for the account Administrator.
 string_to_encode+="Password"
 
 # Concatenate each of the character with a null character and encode to base64. The result will be printed out automatically.
