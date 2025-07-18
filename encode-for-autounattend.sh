0024e13bf50f583f34c6d65128a1d0e906ceacd7
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-07-18 22:18:51 +0300

    Remove testing code from encode-for-autounattend

--- encode-for-autounattend.sh
+++ encode-for-autounattend.sh
@@ -10,7 +10,5 @@ echo -ne "\n"
 # TODO: Ask for the account type and change the phrase to concatenate to "AdminPassword" if it's for the account Administrator.
 string_to_encode+="Password"
 
-echo "$string_to_encode" | sed 's/./&\x00/g' > test_var.txt
-
 # Concatenate each of the character with a null character and encode to base64. The result will be printed out automatically.
 echo "$string_to_encode" | tr -d '\n' | sed 's/./&\x00/g' | base64
