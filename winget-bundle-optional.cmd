c75440ba2a8a0f8c4ac95c0c2a3abdb787f0f78a
Bahar KURT <kurtbahartr@users.noreply.github.com>
2025-07-24 00:24:22 -0400

    Don't install Windscribe

--- winget-bundle-optional.cmd
+++ winget-bundle-optional.cmd
@@ -2,7 +2,6 @@ winget update --accept-source-agreements
 start winget install Git.Git --source winget
 start winget install GitHub.cli --source winget
 start winget install Google.Chrome --source winget
-start winget install Windscribe.Windscribe --source winget
 start winget install "Samsung Cloud Assistant" --source msstore --accept-package-agreements
 start winget install "Microsoft To Do" --source msstore --accept-package-agreements
 start winget install Microsoft.PowerToys --source winget
